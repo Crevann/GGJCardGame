72aec7ef02e8c1b7a5daf0e8c73336ac3ddcf4e6
Alessandro Varisco <82876230+il-Grigio@users.noreply.github.com>
2022-01-22 18:00:59 +0100

    ScriptableObjs

---
+++ GGJCardGame/Assets/Scripts/ScriptableObgs/ScriptableMajor.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/MajorScriptableObject", order = 1)]
+public class ScriptableMajor : ScriptableObject
+{
+    [SerializeField] string cardName;
+    [SerializeField] int soulCoinGain, bodyIngotGain;
+    [SerializeField] Texture cardFace;
+    [SerializeField] int reverceSoulCoinGain, reverseBodyIngotGain;
+}
+
