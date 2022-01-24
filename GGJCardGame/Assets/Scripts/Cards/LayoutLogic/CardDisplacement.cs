497c7ff517df26938831af175de4b9d420573093
Stefano Canavesi <ultratails2001@gmail.com>
2022-01-24 02:28:40 +0100

    Working UI

--- GGJCardGame/Assets/Scripts/Cards/LayoutLogic/CardDisplacement.cs
+++ GGJCardGame/Assets/Scripts/Cards/LayoutLogic/CardDisplacement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CardHandUI : MonoBehaviour
+public class CardDisplacement : MonoBehaviour
 {
     private float spacing;
     [SerializeField] private float handSize;
