79ea83440eb8e0b9a78ce2f7cf4115d55ecbb78e
ImmatureOwl <matteopoli2001@gmail.com>
2022-01-26 21:16:30 +0100

    added first card sfx

--- GGJCardGame/Assets/Scripts/GameLogic/PauseLogic/PausePagesManager.cs
+++ GGJCardGame/Assets/Scripts/GameLogic/PauseLogic/PausePagesManager.cs
@@ -6,6 +6,7 @@ public class PausePagesManager : MonoBehaviour
 {
     [SerializeField]
     private List<PausePageAnimations> pages;
+    
     private int activePage;
     private int previousPage;
 
